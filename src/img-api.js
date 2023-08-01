667b38790e43a529913d6eb73f2a8a17f7897ef1
Oleksandr Voloshun <voloshun3337@gmail.com>
2023-08-02 00:14:34 +0200

    work

--- src/img-api.js
+++ src/img-api.js
@@ -1,27 +1,3 @@
-// import axios from 'axios';
-
-// const API_KEY = '38571599-d8bb1b9f57ff42e3a0ab2e61e';
-// const BASE_URL = 'https://pixabay.com/api/';
-
-// export async function fetchImages(query, page = 1) {
-//   try {
-//     const response = await axios.get(BASE_URL, {
-//       params: {
-//         key: API_KEY,
-//         q: query,
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         safesearch: true,
-//         per_page: 40,
-//         page: page,
-//       },
-//     });
-//     return response.data;
-//   } catch (error) {
-//     throw new Error('Error fetching images:', error);
-//   }
-// }
-
 import axios from 'axios';
 
 const API_KEY = '38571599-d8bb1b9f57ff42e3a0ab2e61e';
@@ -40,6 +16,11 @@ export async function fetchImages(query, page = 1) {
         page: page,
       },
     });
+
+    if (response.status >= 400) {
+      throw new Error('API error');
+    }
+
     return response.data;
   } catch (error) {
     throw new Error('Error fetching images:', error);
